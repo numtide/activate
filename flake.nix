28d63cb4daecf0d2f4f16ff937ddaf9039c9b518
zimbatm <zimbatm@zimbatm.com>
2022-12-27 21:51:52 +0100

    flake-parts and formatting

--- flake.nix
+++ flake.nix
@@ -1,11 +1,29 @@
 {
   description = "activate - a small resource converging tool";
 
-  outputs = { self, nixpkgs }: {
-    devShells.x86_64-linux.default =
-      with nixpkgs.legacyPackages.x86_64-linux;
-      mkShell {
-        packages = [ go ];
+  inputs.flake-parts.url = "github:hercules-ci/flake-parts";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.treefmt-config.url = "github:numtide/treefmt-nix";
+
+  outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = nixpkgs.lib.systems.flakeExposed;
+      imports = [
+        inputs.treefmt-config.flakeModule
+      ];
+      perSystem = { self', lib, config, pkgs, ... }: {
+        # configure treefmt
+        treefmt.projectRootFile = "flake.nix";
+        treefmt.programs.gofmt.enable = true;
+        treefmt.programs.nixpkgs-fmt.enable = true;
+
+        # invoked by `nix fmt`
+        formatter = config.treefmt.build.wrapper;
+
+        # setup a developer shell
+        devShells.default = pkgs.mkShell {
+          packages = [ pkgs.go ];
+        };
       };
-  };
+    };
 }
